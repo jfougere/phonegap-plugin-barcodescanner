5a054a311eaad9d1aa57951835a28f9be9c55b88
Julien Fougere <julien.fougere@transatel.com>
2023-05-16 12:19:15 +0800

    Switch to embedded zxing + customized android layout to match iOS with a back button

--- src/android/com/transatel/plugins/barcodescanner/BarcodeScanner.java
+++ src/android/com/transatel/plugins/barcodescanner/BarcodeScanner.java
@@ -6,7 +6,7 @@
  * Copyright (c) 2011, IBM Corporation
  * Copyright (c) 2013, Maciej Nux Jaros
  */
-package com.phonegap.plugins.barcodescanner;
+package com.transatel.plugins.barcodescanner;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -26,9 +26,8 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.PluginResult;
 import org.apache.cordova.PermissionHelper;
 
-import com.google.zxing.client.android.CaptureActivity;
-import com.google.zxing.client.android.encode.EncodeActivity;
 import com.google.zxing.client.android.Intents;
+import com.journeyapps.barcodescanner.CaptureActivity;
 
 /**
  * This calls out to the ZXing barcode reader and returns the result.
@@ -39,12 +38,9 @@ public class BarcodeScanner extends CordovaPlugin {
     public static final int REQUEST_CODE = 0x0ba7c;
 
     private static final String SCAN = "scan";
-    private static final String ENCODE = "encode";
     private static final String CANCELLED = "cancelled";
     private static final String FORMAT = "format";
     private static final String TEXT = "text";
-    private static final String DATA = "data";
-    private static final String TYPE = "type";
     private static final String PREFER_FRONTCAMERA = "preferFrontCamera";
     private static final String ORIENTATION = "orientation";
     private static final String SHOW_FLIP_CAMERA_BUTTON = "showFlipCameraButton";
@@ -56,13 +52,10 @@ public class BarcodeScanner extends CordovaPlugin {
     private static final String FORMATS = "formats";
     private static final String PROMPT = "prompt";
     private static final String TEXT_TYPE = "TEXT_TYPE";
-    private static final String EMAIL_TYPE = "EMAIL_TYPE";
-    private static final String PHONE_TYPE = "PHONE_TYPE";
-    private static final String SMS_TYPE = "SMS_TYPE";
 
     private static final String LOG_TAG = "BarcodeScanner";
 
-    private String [] permissions = { Manifest.permission.CAMERA };
+    private final String [] permissions = { Manifest.permission.CAMERA };
 
     private JSONArray requestArgs;
     private CallbackContext callbackContext;
@@ -94,28 +87,7 @@ public class BarcodeScanner extends CordovaPlugin {
         this.callbackContext = callbackContext;
         this.requestArgs = args;
 
-        if (action.equals(ENCODE)) {
-            JSONObject obj = args.optJSONObject(0);
-            if (obj != null) {
-                String type = obj.optString(TYPE);
-                String data = obj.optString(DATA);
-
-                // If the type is null then force the type to text
-                if (type == null) {
-                    type = TEXT_TYPE;
-                }
-
-                if (data == null) {
-                    callbackContext.error("User did not specify data to encode");
-                    return true;
-                }
-
-                encode(type, data);
-            } else {
-                callbackContext.error("User did not specify data to encode");
-                return true;
-            }
-        } else if (action.equals(SCAN)) {
+        if (action.equals(SCAN)) {
 
             //android permission auto add
             if(!hasPermisssion()) {
@@ -136,75 +108,66 @@ public class BarcodeScanner extends CordovaPlugin {
 
         final CordovaPlugin that = this;
 
-        cordova.getThreadPool().execute(new Runnable() {
-            public void run() {
+        cordova.getThreadPool().execute(() -> {
 
-                Intent intentScan = new Intent(that.cordova.getActivity().getBaseContext(), CaptureActivity.class);
-                intentScan.setAction(Intents.Scan.ACTION);
-                intentScan.addCategory(Intent.CATEGORY_DEFAULT);
+            Intent intentScan = new Intent(that.cordova.getActivity().getBaseContext(), CustomScannerActivity.class);
+            intentScan.setAction(Intents.Scan.ACTION);
+            intentScan.addCategory(Intent.CATEGORY_DEFAULT);
 
-                // add config as intent extras
-                if (args.length() > 0) {
+            // add config as intent extras
+            if (args.length() > 0) {
 
-                    JSONObject obj;
-                    JSONArray names;
-                    String key;
-                    Object value;
+                JSONObject obj;
+                JSONArray names;
+                String key;
+                Object value;
 
-                    for (int i = 0; i < args.length(); i++) {
+                for (int i = 0; i < args.length(); i++) {
 
+                    try {
+                        obj = args.getJSONObject(i);
+                    } catch (JSONException e) {
+                        Log.i("CordovaLog", e.getLocalizedMessage());
+                        continue;
+                    }
+
+                    names = obj.names();
+                    for (int j = 0; j < names.length(); j++) {
                         try {
-                            obj = args.getJSONObject(i);
+                            key = names.getString(j);
+                            value = obj.get(key);
+
+                            if (value instanceof Integer) {
+                                intentScan.putExtra(key, (Integer) value);
+                            } else if (value instanceof String) {
+                                intentScan.putExtra(key, (String) value);
+                            }
+
                         } catch (JSONException e) {
                             Log.i("CordovaLog", e.getLocalizedMessage());
-                            continue;
-                        }
-
-                        names = obj.names();
-                        for (int j = 0; j < names.length(); j++) {
-                            try {
-                                key = names.getString(j);
-                                value = obj.get(key);
-
-                                if (value instanceof Integer) {
-                                    intentScan.putExtra(key, (Integer) value);
-                                } else if (value instanceof String) {
-                                    intentScan.putExtra(key, (String) value);
-                                }
-
-                            } catch (JSONException e) {
-                                Log.i("CordovaLog", e.getLocalizedMessage());
-                            }
-                        }
-
-                        intentScan.putExtra(Intents.Scan.CAMERA_ID, obj.optBoolean(PREFER_FRONTCAMERA, false) ? 1 : 0);
-                        intentScan.putExtra(Intents.Scan.SHOW_FLIP_CAMERA_BUTTON, obj.optBoolean(SHOW_FLIP_CAMERA_BUTTON, false));
-                        intentScan.putExtra(Intents.Scan.SHOW_TORCH_BUTTON, obj.optBoolean(SHOW_TORCH_BUTTON, false));
-                        intentScan.putExtra(Intents.Scan.TORCH_ON, obj.optBoolean(TORCH_ON, false));
-                        intentScan.putExtra(Intents.Scan.SAVE_HISTORY, obj.optBoolean(SAVE_HISTORY, false));
-                        boolean beep = obj.optBoolean(DISABLE_BEEP, false);
-                        intentScan.putExtra(Intents.Scan.BEEP_ON_SCAN, !beep);
-                        if (obj.has(RESULTDISPLAY_DURATION)) {
-                            intentScan.putExtra(Intents.Scan.RESULT_DISPLAY_DURATION_MS, "" + obj.optLong(RESULTDISPLAY_DURATION));
-                        }
-                        if (obj.has(FORMATS)) {
-                            intentScan.putExtra(Intents.Scan.FORMATS, obj.optString(FORMATS));
-                        }
-                        if (obj.has(PROMPT)) {
-                            intentScan.putExtra(Intents.Scan.PROMPT_MESSAGE, obj.optString(PROMPT));
-                        }
-                        if (obj.has(ORIENTATION)) {
-                            intentScan.putExtra(Intents.Scan.ORIENTATION_LOCK, obj.optString(ORIENTATION));
                         }
                     }
 
+                    intentScan.putExtra(Intents.Scan.CAMERA_ID, obj.optBoolean(PREFER_FRONTCAMERA, false) ? 1 : 0);
+                    boolean beep = obj.optBoolean(DISABLE_BEEP, false);
+                    intentScan.putExtra(Intents.Scan.BEEP_ENABLED, !beep);
+                    if (obj.has(FORMATS)) {
+                        intentScan.putExtra(Intents.Scan.FORMATS, obj.optString(FORMATS));
+                    }
+                    if (obj.has(PROMPT)) {
+                        intentScan.putExtra(Intents.Scan.PROMPT_MESSAGE, obj.optString(PROMPT));
+                    } else {
+                      intentScan.putExtra(Intents.Scan.PROMPT_MESSAGE, "");
+                    }
+                    intentScan.putExtra(Intents.Scan.ORIENTATION_LOCKED, obj.optString(ORIENTATION));
                 }
 
-                // avoid calling other phonegap apps
-                intentScan.setPackage(that.cordova.getActivity().getApplicationContext().getPackageName());
-
-                that.cordova.startActivityForResult(that, intentScan, REQUEST_CODE);
             }
+
+            // avoid calling other phonegap apps
+            intentScan.setPackage(that.cordova.getActivity().getApplicationContext().getPackageName());
+
+            that.cordova.startActivityForResult(that, intentScan, REQUEST_CODE);
         });
     }
 
@@ -228,7 +191,6 @@ public class BarcodeScanner extends CordovaPlugin {
                 } catch (JSONException e) {
                     Log.d(LOG_TAG, "This should never happen");
                 }
-                //this.success(new PluginResult(PluginResult.Status.OK, obj), this.callback);
                 this.vibrate();
                 this.callbackContext.success(obj);
             } else if (resultCode == Activity.RESULT_CANCELED) {
@@ -240,30 +202,11 @@ public class BarcodeScanner extends CordovaPlugin {
                 } catch (JSONException e) {
                     Log.d(LOG_TAG, "This should never happen");
                 }
-                //this.success(new PluginResult(PluginResult.Status.OK, obj), this.callback);
                 this.callbackContext.success(obj);
             } else {
-                //this.error(new PluginResult(PluginResult.Status.ERROR), this.callback);
                 this.callbackContext.error("Unexpected error");
             }
         }
-    }
-
-    /**
-     * Initiates a barcode encode.
-     *
-     * @param type Endoiding type.
-     * @param data The data to encode in the bar code.
-     */
-    public void encode(String type, String data) {
-        Intent intentEncode = new Intent(this.cordova.getActivity().getBaseContext(), EncodeActivity.class);
-        intentEncode.setAction(Intents.Encode.ACTION);
-        intentEncode.putExtra(Intents.Encode.TYPE, type);
-        intentEncode.putExtra(Intents.Encode.DATA, data);
-        // avoid calling other phonegap apps
-        intentEncode.setPackage(this.cordova.getActivity().getApplicationContext().getPackageName());
-
-        this.cordova.getActivity().startActivity(intentEncode);
     }
 
     /**
---
+++ src/android/com/transatel/plugins/barcodescanner/CustomScannerActivity.java
@@ -0,0 +1,17 @@
+package com.transatel.plugins.barcodescanner;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+import com.transatel.selfcare.ubigi.R;
+
+/**
+ * Custom Scannner Activity extending from Activity to display a custom layout form scanner view.
+ */
+public class CustomScannerActivity extends CaptureActivity {
+
+  @Override
+  protected DecoratedBarcodeView initializeContent() {
+    setContentView(R.layout.custom_scanner);
+    return findViewById(R.id.zxing_barcode_scanner);
+  }
+}
